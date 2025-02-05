5e9c4bbcc3981e41c4089d0e8a2803583ac248c2
Elmar465 <elmar465@gmail.com>
2025-02-05 15:03:25 -0500

    GatewayAndEurekaDependencyConnection

---
+++ src/test/java/com/example/springgatewayexample/SpringGatewayExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springgatewayexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGatewayExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
