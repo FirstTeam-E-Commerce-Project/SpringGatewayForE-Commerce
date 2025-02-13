21ac4b07311fafb0589a4987d32bb3ccae9beee3
Elmar465 <elmar465@gmail.com>
2025-02-12 20:44:54 -0500

    minor_update

--- src/main/java/com/example/springgatewayexample/SpringGatewayApplication.java
+++ src/main/java/com/example/springgatewayexample/SpringGatewayApplication.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class SpringGatewaypplication {
+public class SpringGatewayApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringGatewaypplication.class, args);
+        SpringApplication.run(SpringGatewayApplication.class, args);
     }
 
 }
